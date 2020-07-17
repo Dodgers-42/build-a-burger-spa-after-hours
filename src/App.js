26ae619ff94d5bd15867eb5209e1629ad385a7ae
Dodger-42 <kevin.bing.barber@gmail.com>
2020-07-16 19:43:36 -0700

    first

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Route, Switch } form 'react-router-dom';
+import NavBar from ./components/NavBar;
+import HomePage from ./components/HomePage;            
+import BurgerForm ./components/BurgerForm;
 
 function App() {
   return (
     <div>
+    <NavBar />
+    <Switch>
+      <Route path='/buildaburger'>
+        <BurgerForm />
+        </Route>
+        </Route exact path='/'>
+        <Home Page />
+        </Route>
+        </Switch>
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavBar = props => {
+    return (
+        <Navigation>
+            <Link to='/'>Home</Link>
+        </Navigation>
+
+    );
+}
+
+const 
